41a9978bd470ff7b62de27e51039ffab63fbeff2
Nutthaka Tanthanuch <nutthaka.tanthanuch@news.com.au>
2014-12-02 22:44:26 +1100

    added dummy test

---
+++ spec/app.spec.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var expect = require("chai").expect;
+
+describe("recipe api", function() {
+
+    it("should return http status 200", function() {
+        expect(1).to.equal(1);
+    });
+
+});
