7f72a72f40a6f410ef193c3ddc14571aa8259e4b
Nutthaka Tanthanuch <nutthaka.tanthanuch@news.com.au>
2014-12-03 23:34:55 +1100

    added test to check server response

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,11 +1,33 @@
 "use strict";
 
 var expect = require("chai").expect;
+var app = require("../app");
+var request = require("request");
 
-describe("recipe api", function() {
+describe("recipe api", function () {
+    var port;
+    var server;
 
-    it("should return http status 200", function() {
-        expect(1).to.equal(1);
+    before(function (done) {
+        server = app.listen(0, function () {
+            port = server.address().port;
+            console.log("Server starts at port: " + port);
+            done();
+        });
     });
 
+    it("should return http status 200", function (done) {
+        var url = "http://localhost:" + port + "/recipe/chicken-skewers";
+        request.get(url, function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    after(function (done) {
+        server.close(function () {
+            console.log("Server stopped");
+            done();
+        });
+    });
 });
