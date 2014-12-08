de7c3e656e58f89bb517d82424539b10ed5d26a1
Nutthaka Tanthanuch <nutthaka.tanthanuch@news.com.au>
2014-12-08 23:15:21 +1100

    added test to check list of recipes

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -7,19 +7,33 @@ var request = require("request");
 describe("recipe api", function () {
     var port;
     var server;
+    var baseRecipeUrl;
 
     before(function (done) {
         server = app.listen(0, function () {
             port = server.address().port;
-            console.log("Server starts at port: " + port);
+            baseRecipeUrl = "http://localhost:" + port + "/recipes";
+            console.log("Server started at port: " + port);
             done();
         });
     });
 
     it("should return http status 200", function (done) {
-        var url = "http://localhost:" + port + "/recipes/chicken-skewers";
-        request.get(url, function (error, response, body) {
+        request.get(baseRecipeUrl + "/chicken-skewers", function (error, response, body) {
             expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it("should return a list of recipes", function (done) {
+        request.get({
+            url: baseRecipeUrl,
+            json: true
+        }, function (error, response, body) {
+            console.log(typeof body);
+            console.log(body.length);
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.have.length.above(0);
             done();
         });
     });
